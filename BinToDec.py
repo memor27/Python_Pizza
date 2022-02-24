a3a051876f502b690e90bd6fb77e6a872f55844b
memor27 <100342106+memor27@users.noreply.github.com>
2022-02-24 09:26:42 +0100

    Update BinToDec.py

--- BinToDec.py
+++ BinToDec.py
@@ -12,3 +12,30 @@ for k in range(lunghezza):
     num_bin = int(num_bin/10)
 
 print("Il tuo numero in decimale è: ", num_dec)
+
+
+#Input ottetti binario
+
+ott1 = "11111111"
+ott2 = "11111111"
+ott3 = "11111111"
+ott4 = "0"
+
+#Funzione di conversione binario a decimale
+
+def bintodec (ottetto):
+    lunghezza = len(str(ottetto))
+    num_dec = 0
+    esponenz = 1
+
+    for k in range(lunghezza):
+        convers = ottetto % 10
+        num_dec = num_dec + (convers * esponenz)
+        esponenz = esponenz * 2
+        ottetto = int(ottetto/10)
+    return num_dec
+
+
+ip = [bintodec (int(ott1)), bintodec (int(ott2)), bintodec (int(ott3)), bintodec (int(ott4))]
+
+print("Il tuo numero in decimale è: ", ip)
